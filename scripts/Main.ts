94b17680b7284873ae57b77f4db53240933c554c
AlexOrlovsky <oaowork1@ya.ru>
2019-02-17 17:45:00 +0300

    Added code
    Added code

---
+++ scripts/Main.ts
@@ -0,0 +1,31 @@
+﻿class Main
+{
+	private col:Collection;
+	private graph:Graph;
+	private snd:Sounds;
+	private state:States;
+	private ui:UI;
+	
+	constructor()
+	{
+		this.col=Collection.getInstance();
+		this.graph = Graph.getInstance();
+		this.snd = new Sounds();
+		this.state = new States();
+		this.ui = new UI();
+	}	
+	
+	public gameLoop():void
+	{
+		
+		if (col.loadGraph && col.loadSounds)
+		{
+			this.state.go(items);
+			this.graph.go();			
+			this.ui.go();
+			this.snd.toPlay();
+		}
+		this.graph.rendAll(); 
+		requestAnimationFrame(this.gameLoop.bind(this)); 
+	}
+}
